da3b4c4addad70894ca39fb8abdab58d5261fd48
Wojciech Palacz <wojciechpalacz@gmail.com>
2024-06-07 02:30:54 +0200

    addding catch in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,58 @@ import { BooksList } from "./container/booksList/booksList";
 import { AddBookForm } from "./container/addBookForm/addBookForm";
 /* ALL PROMISES */
 import {
-  getAllBooks,
-  createBook,
-  readBookById,
-  updateBookById,
-  deleteBookById,
+    getAllBooks,
+    createBook,
+    // not mentioned in README.md
+    readBookById,
+    updateBookById,
+    deleteBookById,
 } from "./api";
 
-/* USE THIS HOOK TO HANDLE PROMISES */
 import { useFetch } from "./utils/useFetch";
 
 export default function App() {
-  const { data, isLoading, isError, retry } = useFetch(getAllBooks);
+    const { data, isLoading, isError, retry } = useFetch(getAllBooks);
 
-  return (
-    <div className="container">
-      <h1>Welcome to Adesso React Test</h1>
-      <BooksList
-        data={data}
-        isLoading={isLoading}
-        isError={isError}
-        onRemove={async (id) => {
-          try {
-            await deleteBookById(id);
-          } finally {
-            retry();
-          }
-        }}
-        onModify={async (id, title, description) => {
-          try {
-            await updateBookById(id, { title, description });
-          } finally {
-            retry();
-          }
-        }}
-      />
-      <AddBookForm
-        isLoading={isLoading}
-        isError={isError}
-        onAdd={async (title, description) => {
-          await createBook({ title, description });
-          retry();
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Welcome to Adesso React Test</h1>
+            <BooksList
+                data={data}
+                isLoading={isLoading}
+                isError={isError}
+                onRemove={async (id) => {
+                    try {
+                        await deleteBookById(id);
+                    } catch {
+                        // In case of error
+                    } finally {
+                        retry();
+                    }
+                }}
+                onModify={async (id, title, description) => {
+                    try {
+                        await updateBookById(id, { title, description });
+                    } catch {
+                        // In case of error
+                    } finally {
+                        retry();
+                    }
+                }}
+            />
+            <AddBookForm
+                isLoading={isLoading}
+                isError={isError}
+                onAdd={async (title, description) => {
+                    try {
+                        await createBook({ title, description });
+                    } catch {
+                        // In case of error
+                    } finally {
+                        retry();
+                    }
+                }}
+            />
+        </div>
+    );
 }
